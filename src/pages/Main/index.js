81579c0bb8a350e87bc47a8193f0586559462051
matheus <matheusmmmp@hotmail.com>
2020-03-20 11:58:35 -0300

    Fisrt

---
+++ src/pages/Main/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
+
+export default class Main extends Component {
+    state = {
+        newRepo:'',
+    };
+
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value});
+    };
+
+    handleInputChange = e => {
+            e.preventDefault();
+            console.log(this.state.newRepo);
+    };
+    render() {
+        const { newRepo } = this.state;
+        return (
+            <Container>
+                <h1>
+                    <FaGithubAlt />
+                    Repositorios
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input type="text"
+                     placeholder="Adicionar repositório"
+                     onChange={this.handleInputChange}
+                     />
+
+                    <SubmitButton>
+                        <FaPlus color="#FFF" size={14} />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
+}
